5b1c9a05373f99aa41291f9c1f31620cd00bb44a
Javier Castilla <jcastp@pm.me>
2026-06-08 20:28:53 +0200

    org-journal added

--- init.el
+++ init.el
@@ -163,6 +163,7 @@
        (org                             ; organize your plain life in plain text
         +dragndrop
         ;; +pretty
+        +journal
         )
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
